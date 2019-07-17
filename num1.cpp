a4c7ec25bf577d680fec4eb013ebfd9de202b94a
fuermobai <fuermobai@gmail.com>
2019-07-17 22:04:41 +0800

    add v1 times v2 result

--- num1.cpp
+++ num1.cpp
@@ -2,9 +2,9 @@
 int main()
 {
     std::cout << "Hello,World" << std::endl;
-//    int v1 = 0,v2 = 0;
-//    std::cin >> v1 >> v2;
-//    std::cout << "The sum of " << v1 << " and " << v2 << " is "<< v1 +v2 <<std::endl;
-//    
+    int v1 = 0,v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " and " << v2 << " is "<< v1 +v2 << "两数之积为：" << v1 * v2 << std::endl;
+    
     return 0;
 }
