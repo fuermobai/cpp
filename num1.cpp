6bbad9b254015de892f653f8ea6e256a3f7136e4
fuermobai <fuermobai@gmail.com>
2019-07-17 22:53:35 +0800

    输入两个整数，输出指定范围内所有整数(限于第一个数<第二个数)

--- num1.cpp
+++ num1.cpp
@@ -4,10 +4,11 @@ Sum of 5 to 100
  */
 int main()
 {
-    int sum = 0,count = 10;
-    while(count >=1){
-        std::cout << count << std::endl;
+    int sum = 0,count = 0;
+    std::cin >> sum >> count;
+    while(count-sum >1){// count - sum > 1 是因为不打印出两个数的边界，1是不打下界，仅大于号 >是不打印上界
         --count;
+        std::cout << count << std::endl;
     }
 //    std::cout<< sum << std::endl;
     return 0;
