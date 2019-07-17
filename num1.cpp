b3c6c10a22a2321f38822aa00852993078262744
fuermobai <fuermobai@gmail.com>
2019-07-17 22:29:13 +0800

    求1到10的和

--- num1.cpp
+++ num1.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
+/*
+Sum of 1 to 10
+ */
 int main()
 {
+    int sum = 0,count = 1;
+    while(count <=10){
+        sum += count;
+        ++count;
+    }
+    std::cout<< sum << std::endl;
+    return 0;
+}
 //    std::cout << "Hello,World" << std::endl;
 //    int v1 = 0,v2 = 0;
 //    std::cin >> v1 >> v2;
 //    std::cout << "The sum of " << v1 << " and " << v2 << " is "<< v1 +v2 << "两数之积为：" << v1 * v2 << std::endl;
 //    
-    std::cout << "/*";
-    std::cout << "*/";
-    std::cout << /* "*/" */";
-    std::cout << /* "*/" /* "/*" */;
-    return 0;
-}
+//    std::cout << "/*";
+//    std::cout << "*/";
+//    std::cout << /* "*/" */";
+//    std::cout << /* "*/" /* "/*" */;
+
