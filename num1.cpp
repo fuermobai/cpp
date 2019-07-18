c1d9ca0205ec07c38b14bed23a41f3751eb5d6c0
fuermobai <fuermobai@gmail.com>
2019-07-18 12:56:49 +0000

    while循环，cin读取数，输出和

--- num1.cpp
+++ num1.cpp
@@ -4,11 +4,13 @@ list numbers between n1 and n2 (n2>n1)
  */
 int main()
 {
-    int n1 = 0,n2 = 0;
-    std::cin >> n1 >> n2;
-    for(int i = n1+1;i < n2 ;++i)
-        std::cout << i << std::endl;
-        //std::cout << "输出内容" << std::endl;
+    int sum = 0,val = 0;
+    //std::cin >> n1 >> n2;
+    while (std::cin >> val)
+    {
+        sum += val;
+        std::cout << sum << std::endl;
+    }
     //std::cout << sum << std::endl;
     return 0;
 }
