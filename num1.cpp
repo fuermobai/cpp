a5f7955afbf177824757bc5c0b4b9b0637b783d9
fuermobai <fuermobai@gmail.com>
2019-07-19 15:50:53 +0000

    1.21

--- num1.cpp
+++ num1.cpp
@@ -3,11 +3,20 @@
 /*
 list numbers between n1 and n2 (n2>n1)
  */
-int main(int argc, char const *argv[])
+int main()
 {
-    Sales_item books;
-    std::cin >> books;
-    std::cout << books << std::endl;
-    return 0;
+    Sales_item item1,item2;
+    std::cin >> item1 >> item2;
+    std::cout << item1 + item2 << std::endl;
+    if (item1.isbn() == item2.isbn())
+    {
+        std::cout << item1 + item2 << std::endl;
+        return 0;
+    }
+    else
+    {
+        std::cerr << "Data must refer to same ISBN" <<std::endl;
+        return -1;
+    }
 }
 
