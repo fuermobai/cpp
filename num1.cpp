5464fdd3bba92691b972ee8e01f9cc203174bfa3
fuermobai <fuermobai@gmail.com>
2019-07-21 14:01:56 +0000

    练习1.22,做的不是很好，程序思维有待加强

--- num1.cpp
+++ num1.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include "./Sales_item.h"
 /*
@@ -5,18 +6,26 @@ list numbers between n1 and n2 (n2>n1)
  */
 int main()
 {
-    Sales_item item1,item2;
-    std::cin >> item1 >> item2;
-    //std::cout << item1 + item2 << std::endl;
-    if (item1.isbn() == item2.isbn())
+    Sales_item Curritem,Newitem;
+    if (std::cin >> Curritem)//使用if语句来判断当前输入是否为真，真则继续，假则跳出
     {
-        std::cout << item1 + item2 << std::endl;
-        return 0;
+        int count = 1;//当前输入的第一个内容计数
+        while (std::cin >> Newitem) //输入新内容，并且与前一条输入内容进行比较
+        {
+            if (Curritem.isbn()==Newitem.isbn())// 判断是否相同内容
+            {
+                ++count;
+            }
+            else
+            {
+                std::cout << Curritem << count << std::endl;
+                Curritem=Newitem; //不是相同内容则将Curritem替换为最新内容
+                count=1; //重置count为1，重新计数开始
+            }
+            
+        }
+        std::cout << Curritem << count << std::endl;//将最后一个内容和次数打印
     }
-    else
-    {
-        std::cerr << "Data must refer to same ISBN" <<std::endl;
-        return -1;
-    }
+    return 0;
 }
 
