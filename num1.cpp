f40c58b960d7af596a68cde45d9d54530fb0c071
fuermobai <fuermobai@gmail.com>
2019-07-28 14:54:02 +0000

    指针的引用

--- num1.cpp
+++ num1.cpp
@@ -8,8 +8,10 @@
 int main()
 {
     int i = 42;
-    int *pi = &i;
-    *pi = *pi * *pi;
-    std::cout << i << "\t" << pi << std::endl;
-    return 0;
+    int *p;
+    int *&r = p;// r是指针p的引用，所以r的类型是int类型的*指针引用
+
+    r = &i;
+    *r = 0;
+
 }
