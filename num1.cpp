374d9ac3e4320fd5e8a49c95b30c9951b35dc731
fuermobai <fuermobai@gmail.com>
2019-07-28 03:25:14 +0000

    重新修改了练习2.14

--- num1.cpp
+++ num1.cpp
@@ -4,13 +4,13 @@
 /*
 
  */
-int i = 42;
 int main()
 {
     int i = 100,sum = 0;
     for (int i = 0; i != 10; ++i)
     {
         sum += 1;  
+        std::cout << i << std::endl;//每次循环必输出此语句，无法仅输出最终的i的值
     }
     std::cout << i << " " << sum << "\n" << std::endl;
     return 0;
