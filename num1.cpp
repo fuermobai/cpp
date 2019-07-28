c21b253ee788defec4fbdf8ee512d022eacf3b08
fuermobai <fuermobai@gmail.com>
2019-07-28 12:54:21 +0000

    空指针举例，分别赋值nullptr、0、NULL

--- num1.cpp
+++ num1.cpp
@@ -7,14 +7,12 @@
  */
 int main()
 {
-    int ival = 42;
-    int *p = &ival;
-    // p 是指向 ival的int型指针，*p代表了取指针p指向的对象（ival）的值
-    std::cout << *p << "\t" << p <<std::endl;
-    int  *a = p;
-    //a为对象p的指针，p本身是指针，其值是p指针指向对象ival的地址，所以*a的值为ival的地址
-    std::cout << a << "\t" << &a <<std::endl;
-    *a = 10;//直接给解引用的指针a赋值10
-    std::cout << a << "\t" << *a <<std::endl;
+    int *p1 = nullptr;
+    int *p2 = 0;
+    int *p3 = NULL;
+    std::cout << p1 << "\t" << p2 << "\t" << p3 << std::endl;
+    // 0 0 0
+    std::cout << *p1 << "\t" << *p2 << "\t" << *p3 << std::endl;
+    // [1]  随机值  core dumped
     return 0;
 }
