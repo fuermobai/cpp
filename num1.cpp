e7cffabe2b78507b585001b453cf027d0dc8a770
fuermobai <fuermobai@gmail.com>
2019-07-28 08:03:10 +0000

    修改了readme，并对引用程序进行练习

--- num1.cpp
+++ num1.cpp
@@ -6,12 +6,8 @@
  */
 int main()
 {
-    int ival = 1024;
-    int &refVal = ival;// refVal 是 ival 的新名字，也即refVal是ival的引用。
-    std::cout << ival << " " << refVal << std::endl;
-    //int &refVal2;
-    refVal = 2;//可以通过引用给原对象赋值，即2传递给引用名 refVal 然后传递给实际对象ival，ival的值为2
-    int ii = refVal;//也可以使用引用给别的对象赋值，ii被初始化为ival的值
-    std::cout << refVal << " " << ii << std::endl;
+    int i;
+    int &val = i;
+    std::cout << i << " " << val << std::endl;
     return 0;
 }
