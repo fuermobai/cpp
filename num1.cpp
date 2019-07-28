cd9eabce5f228958aac2bb6728721a0e4ce700e3
fuermobai <fuermobai@gmail.com>
2019-07-28 03:08:07 +0000

    2.2.4节练习 2.14

--- num1.cpp
+++ num1.cpp
@@ -4,16 +4,14 @@
 /*
 
  */
-int reused = 42;
+int i = 42;
 int main()
 {
-    int unique = 0;
-    std::cout << reused << "\t" << unique << std::endl;
-    //在内部定义reused前输出的为全局的reused值
-    int reused = 0;
-    std::cout << reused <<  "\t" << unique << std::endl;
-    //在main函数内部的作用域输出，reused为0
-    std::cout << ::reused << "\t" << unique <<std::endl;
-    //reused 前添加了::表示覆盖了当前的作用域规则，即回到了全局作用域，因为全局作用域没有自己的名字，故::前面为空
+    int i = 100,sum = 0;
+    for (int i = 0; i != 10; ++i)
+    {
+        sum += 1;  
+    }
+    std::cout << i << " " << sum << "\n" << std::endl;
     return 0;
 }
