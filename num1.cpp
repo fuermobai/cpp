841459e457a1d44db37a34ae2e01141adb15459b
fuermobai <fuermobai@gmail.com>
2019-07-28 12:40:49 +0000

    增加了关于指针方面的注释，并添加了指针的指针的直观且简单的举例

--- num1.cpp
+++ num1.cpp
@@ -6,8 +6,12 @@
  */
 int main()
 {
-    int i;
-    int &val = i;
-    std::cout << i << " " << val << std::endl;
+    int ival = 42;
+    int *p = &ival;
+    // p 是指向 ival的int型指针，*p代表了取指针p指向的对象（ival）的值
+    std::cout << *p << "\t" << p <<std::endl;
+    int  *a = p;
+    //a为对象p的指针，p本身是指针，其值是p指针指向对象ival的地址，所以*a的值为ival的地址
+    std::cout << a << "\t" << &a <<std::endl;
     return 0;
 }
