18b605fac2a5fd1a5c605a80a6d91ac10ce43370
fuermobai <fuermobai@gmail.com>
2019-07-28 07:37:15 +0000

    引用即别名，可以使用引用给别的新的对象初始化、赋值等等

--- num1.cpp
+++ num1.cpp
@@ -6,12 +6,12 @@
  */
 int main()
 {
-    int i = 100,sum = 0;
-    for (int i = 0; i != 10; ++i)
-    {
-        sum += 1;  
-        std::cout << i << std::endl;//每次循环必输出此语句，无法仅输出最终的i的值
-    }
-    std::cout << i << " " << sum << "\n" << std::endl;
+    int ival = 1024;
+    int &refVal = ival;// refVal 是 ival 的新名字，也即refVal是ival的引用。
+    std::cout << ival << " " << refVal << std::endl;
+    //int &refVal2;
+    refVal = 2;//可以通过引用给原对象赋值，即2传递给引用名 refVal 然后传递给实际对象ival，ival的值为2
+    int ii = refVal;//也可以使用引用给别的对象赋值，ii被初始化为ival的值
+    std::cout << refVal << " " << ii << std::endl;
     return 0;
 }
