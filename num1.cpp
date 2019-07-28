224c48384f47bf4f88775522006c492c61cd1108
fuermobai <fuermobai@gmail.com>
2019-07-28 13:58:33 +0000

    2.3.2练习 2.20

--- num1.cpp
+++ num1.cpp
@@ -7,12 +7,9 @@
  */
 int main()
 {
-    int *p1 = nullptr;
-    int *p2 = 0;
-    int *p3 = NULL;
-    std::cout << p1 << "\t" << p2 << "\t" << p3 << std::endl;
-    // 0 0 0
-    std::cout << *p1 << "\t" << *p2 << "\t" << *p3 << std::endl;
-    // [1]  随机值  core dumped
+    int i = 42;
+    int *pi = &i;
+    *pi = *pi * *pi;
+    std::cout << i << "\t" << pi << std::endl;
     return 0;
 }
