d247f3c596909c1fe5668a18a772d676d44f32bc
fuermobai <fuermobai@gmail.com>
2019-08-17 12:28:00 +0000

    练习2.33（无法编译成功）

--- num1.cpp
+++ num1.cpp
@@ -9,15 +9,27 @@ int main()
 {
     int i = 0,&r = i;
     auto a = r;
+    a = 42;
+    std::cout << i << " " << a << std::endl;
     const int ci = i,&cr = ci;
     auto b = ci;
+    b = 42;
+    std::cout << b << " " << ci << std::endl;
     auto c = cr;
+    c = 42;
+    std::cout << c << " " << cr << " " << ci <<std::endl;
     auto d = &i;
+    //d = 42;
+    std::cout << d << " " << i << std::endl;
     auto e = &ci;
+    e = 42;//常量指针不能再被赋值，只能变动地址
+    std::cout << e << " " << ci << std::endl;
     
     const auto f = ci;
     auto &g = ci;
-    auto &h = 42;
+    //auto &h = 42;
     const auto &j = 42;
-    auto k = ci,
+    auto k = ci,&l = i;
+    auto &m = ci,*p = &ci;
+    //auto &n =i,*p2 = &ci;
 }
