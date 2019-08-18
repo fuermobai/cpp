e83587d282ffcafaf8966cb3288d52fff81ded82
fuermobai <fuermobai@gmail.com>
2019-08-18 14:30:46 +0000

    string 对象第一次

--- num1.cpp
+++ num1.cpp
@@ -4,36 +4,10 @@
 #include <string>
 #include <cstdlib>
 
-
 int main()
 {
-    Sales_data book1,book2;
-    // 输入每本书的书号、售卖数量、售卖单价
-    std::cin >> book1.bookNo >> book1.units_sold >> book1.price;
-    std::cin >> book2.bookNo >> book2.units_sold >> book2.price;
-    // 计算每本书的收入
-    book1.revenue = book1.units_sold * book1.price;
-    book2.revenue = book2.units_sold * book2.price;
-    // 只有当书号一样时，即同一本书时，才计算总价格
-    if (book1.bookNo == book2.bookNo)
-    {
-        unsigned totalCNT = book1.units_sold + book2.units_sold;
-        double totalrevenue = book1.revenue + book2.revenue;
-        std::cout << book1.bookNo << " " << totalCNT << " " << totalrevenue << " ";
-        if (totalCNT != 0)
-        {
-            std::cout << totalrevenue/totalCNT << std::endl;
-        }
-        else
-        {
-            std::cout << "(没卖出去，草T -T)" << std::endl;
-        }
-        return 0;
-    }
-    else
-    {
-        std::cerr << "Data must refer to the same ISBN" << std::endl;
-        return -1;
-    }
+    std::string s;
+    std::cin >> s;
+    std::cout << s << std::endl;
     return 0;
 }
