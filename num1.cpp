961883f36df787b7c6fda5e2ce0f89f313b5e415
fuermobai <fuermobai@gmail.com>
2019-08-18 15:44:47 +0000

    练习3.2

--- num1.cpp
+++ num1.cpp
@@ -6,8 +6,11 @@
 
 int main()
 {
-    std::string s;
-    std::cin >> s;
-    std::cout << s << std::endl;
+    std::string line;
+    while (getline(std::cin,line,' '))//通过一个空格，控制输入内容为第一个空格前的内容
+    {
+        if (!line.empty())
+            std::cout << line << std::endl;
+    }
     return 0;
 }
