5c0f41f8234e2defe60c06a7e004760f7edc64ed
fuermobai <fuermobai@gmail.com>
2019-08-22 08:45:41 +0800

    练习3.14

--- num1.cpp
+++ num1.cpp
@@ -5,17 +5,19 @@
 #include <cstdlib>
 #include <typeinfo>
 #include <cctype>
+#include <vector>
 
 int main()
 {
-    std::string s("Hello World!!!My name is Porco,which means A 'Red' + 'Pig' in Chinese @-@");
-    std::cout << s << std::endl;
-    for (auto &i : s)
+    int number;
+    std::vector<int> numbers;
+    while (std::cin >> number)
     {
-        if (ispunct(i))//此处使用了替换方案，也可以直接判断是否是标点后，将其合到一个string对象中去
-        {
-            i = ' ';//空字符，是使用单引号' ‘ 而不是双引号" "
-        }
+        numbers.push_back(number);
     }
-    std::cout << s << std::endl;
+    for (auto i = 0; i < numbers.size(); ++i)
+    {
+        std::cout << numbers[i] ; 
+    }
+    std::cout << "\n" ;
 }
