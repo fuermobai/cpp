2565f7a64873aec220806845239465cac021f80e
fuermobai <fuermobai@gmail.com>
2019-08-23 16:14:04 +0800

    3.20过程程序

--- num1.cpp
+++ num1.cpp
@@ -9,26 +9,6 @@
 
 int main()
 {
-    std::string word;
-    std::vector<std::string> words;
-    while (std::cin >> word)
-    {
-        words.push_back(word);
-    }
-    //Linux可以使用Ctrl + D来终止字符输入并输出
-
-    for (auto i : words)//将for循环输出words内容，改为for range输出
-    {
-        if(!i.empty())
-            for (int item = 0; item < i.size(); ++item)
-            {
-                i[item] = toupper(i[item]);
-            }
-        std::cout << i << std::endl;
-    }
-   // for (auto i = 0; i < words.size(); ++i)
-   // {
-   //     std::cout << words[i] ; 
-   // }
-   // std::cout << "\n" ;
+    std::vector<int> numbers;
+    int number;
 }
