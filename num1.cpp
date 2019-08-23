f490df95b738a06742b73a8442d514557c3ae1c1
fuermobai <fuermobai@gmail.com>
2019-08-23 12:10:37 +0000

    完善了练习3.20

--- num1.cpp
+++ num1.cpp
@@ -16,7 +16,12 @@ int main()
     std::cout << numbers.size() << std::endl;
     for (auto i = 0; i < numbers.size(); ++i)
     {
-        result += (numbers[i] + numbers[numbers.size()-1-i])/2;//根据高斯公式得出最终结果，要除以2
+        if(i!=numbers.size()-1)
+        {
+        std::cout << numbers[i] + numbers[i+1] << std::endl;//输出所有相邻元素之和
+        }
+        std::cout << numbers[i] << "和" << numbers[numbers.size()-1-i] << "之和为：" << numbers[i] + numbers[numbers.size()-1-i] << "   ";
+        result += (numbers[i] + numbers[numbers.size()-1-i])/2;
     }
-    std::cout << result << std::endl;
+    std::cout << "输出所有数之和：" << result << std::endl;//根据高斯公式得出最终结果，要除以2
 }
