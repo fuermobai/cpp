57ffcff1330915f73a3cb6c018e5e2ad902247a9
fuermobai <fuermobai@gmail.com>
2019-08-24 07:51:16 +0000

    迭代器在for循环中可以不用初始化

--- num1.cpp
+++ num1.cpp
@@ -7,21 +7,12 @@
 
 int main()
 {
-    std::vector<int> numbers;
-    int number,result;
-    while (std::cin >> number)
+    std::vector<std::string> numbers{"hello","my","name","is","Porco","Rosso"};
+    //std::vector<std::string>::iterator i;
+    for (auto i = numbers.cbegin(); i != numbers.cend(); ++i)
     {
-        numbers.push_back(number);
+        std::cout << *i << " ";
     }
-    std::cout << numbers.size() << std::endl;
-    for (auto i = 0; i < numbers.size(); ++i)
-    {
-        if(i!=numbers.size()-1)
-        {
-        std::cout << numbers[i] + numbers[i+1] << std::endl;//输出所有相邻元素之和
-        }
-        std::cout << numbers[i] << "和" << numbers[numbers.size()-1-i] << "之和为：" << numbers[i] + numbers[numbers.size()-1-i] << "   ";
-        result += (numbers[i] + numbers[numbers.size()-1-i])/2;
-    }
-    std::cout << "输出所有数之和：" << result << std::endl;//根据高斯公式得出最终结果，要除以2
+    std::cout << std::endl;
+    return 0;
 }
