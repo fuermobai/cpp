81ebb5b5402264ba56b4585030a6027d3a46d1a2
fuermobai <fuermobai@gmail.com>
2019-08-26 22:51:54 +0800

    练习3.24

--- num1.cpp
+++ num1.cpp
@@ -11,4 +11,26 @@ int main()
 {
     std::vector<int> numbers;
     int number;
+    while (std::cin >> number)
+    {
+        numbers.push_back(number);
+    }
+    //for (auto i : numbers)
+    //{
+    //    std::cout << i*i << " ";
+    //}
+    auto tl1 = numbers.begin();
+    auto tl2 = numbers.end() - 1;
+    //std::cout << (tl1 < tl2) << " " << (numbers.begin() != numbers.end()) << std::endl;
+    while ( tl1 < tl2 )
+    {
+        //std::cout << *tl1+*(tl1 + 1)  << std::endl;
+        std::cout << *tl1 + *tl2 << std::endl;
+        ++tl1;
+        --tl2;
+    }
+    //std::cout << (tl1 < tl2) << " " << (numbers.begin() != numbers.end()) << std::endl;
+    // 之所以不能使用 numbers.begin() != numbers.end() 来作为while循环判断条件，是因为这个条件是一直成立的，即此时while是无限循环，溢出错误
+
+    return 0;
 }
