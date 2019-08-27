df6551e23ef53152375bc6c0ad29ff42c1a4eada
fuermobai <fuermobai@gmail.com>
2019-08-27 21:54:29 +0800

    练习3.32 使用for循环复制数组内容到另一数组

--- num1.cpp
+++ num1.cpp
@@ -6,15 +6,20 @@
 #include <typeinfo>
 #include <cctype>
 #include <vector>
-//练习3.31
+//练习3.32
 int main()
 {
     int numbers[10];
+    int numbers1[10];
     for (size_t i = 0; i < 10; i++)
     {
         numbers[i] = i;
     }
-    for (auto i : numbers)
+    for (size_t i = 0; i < 10; i++)
+    {
+        numbers1[i] = numbers[9-i];
+    }
+    for (auto i : numbers1)
     {
         std::cout << i << " ";
     }
