e12243857b8c26fa7df793a2f8222d3ef78c670a
fuermobai <fuermobai@gmail.com>
2019-08-28 23:04:20 +0800

    练习3.42

--- num1.cpp
+++ num1.cpp
@@ -10,9 +10,13 @@
 //练习3.33
 int main()
 {
-    int numbers[] = {0,1,2,3,4,5,6};
-    std::vector<int> vec(std::begin(numbers),std::end(numbers));
-    for (auto i : vec)
+    std::vector<int> vec{1,2,3,4,5,6,7,8};
+    int a[vec.size()];
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        a[i] = vec[i];
+    }
+    for (auto i : a)
     {
         std::cout << i << " ";
     }
