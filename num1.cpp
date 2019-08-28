6b95aa63e183d5685f64192231958b083fc67ba3
fuermobai <fuermobai@gmail.com>
2019-08-28 08:01:22 +0800

    数组和指针之间的一个定义方式

--- num1.cpp
+++ num1.cpp
@@ -9,19 +9,11 @@
 //练习3.33
 int main()
 {
-    unsigned scores[11];//当scores没有初始化时，其结果会出现不可预知的结果，当然其中包含了正确结果，但是没有覆盖的情况中，尚未测试，但是已经不满足实际使用了
-    unsigned grade;
-    while (std::cin >> grade)
-    {
-        if (grade <= 100)
-        {
-            ++scores[grade/10];
-        }
-    }
-    for (auto i : scores)
-    {
-        std::cout << i << " ";
-    }
+    int arr[]={0,100,2,300,4,5,6,7,8,9};
+    int *p=arr;
+    ++p;
+    int *p1 = p+3;//原来定义指向数组某一单元的指针可以这样初始化，而不是每次都是 int *p1 = &p[4]
+    std::cout << *p1 << std::endl;
     return 0;
 }
         
