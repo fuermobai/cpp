9a7c84595759e702aebac6be0d367a03abe7ea5c
fuermobai <fuermobai@gmail.com>
2019-08-28 21:21:36 +0800

    练习3.39

--- num1.cpp
+++ num1.cpp
@@ -6,14 +6,42 @@
 #include <typeinfo>
 #include <cctype>
 #include <vector>
+#include <cstring>
 //练习3.33
 int main()
 {
-    int arr[]={0,100,2,300,4,5,6,7,8,9};
-    int *p=arr;
-    ++p;
-    int *p1 = p+3;//原来定义指向数组某一单元的指针可以这样初始化，而不是每次都是 int *p1 = &p[4]
-    std::cout << *p1 << std::endl;
+    std::string sentence1 = "aaaaaaaa";
+    std::string sentence2 = "bbbbbbbb";
+    if (sentence1 == sentence2)
+    {
+        std::cout << "Right" << std::endl;
+    }
+    else
+    {
+        std::cout << "Wrong" << std::endl;
+    }
+    
+
+    std::cout << "==================================" << std::endl;
+
+
+    //const char ca1[] = "My name is Porco";
+    const char *ca1("aaaaaaaaaa");
+    //const char ca2[] = "My name is Porco";
+    const char *ca2("aaaaaaaaaa");
+    auto result = std::strcmp(ca1,ca2);
+    if (result == 0)
+    {
+        std::cout << "ca1 is equal with ca2" << std::endl;
+    }
+    else if(result > 0)
+    {
+        std::cout << "ca1 is bigger than ca2" << std::endl;
+    }
+    else
+    {
+        std::cout << "ca2 is bigger than ca1" << std::endl;
+    }
     return 0;
 }
         
