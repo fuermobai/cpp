4b163ef055ead407a106c2dabfd38e257decb1f1
fuermobai <fuermobai@gmail.com>
2020-03-28 18:12:10 +0800

    第六章函数练习，阶乘程序

--- num1.cpp
+++ num1.cpp
@@ -2,20 +2,18 @@
 #include <vector>
 #include <string>
 
-bool isPrefix(const std::vector<int> &v1,const std::vector<int> &v2) {
-    auto item1 = v1.cbegin(),item2 = v2.cbegin();
-    for(;item1 !=v1.cend() && item2 != v2.cend();++item1,++item2){
-        if(*item1 != *item2){
-            break;
-        }
+long fact(int val)
+{
+    long result = 1;
+    while (val > 1)
+    {
+        result *= val--;
     }
-    return item1 == v1.cend() || item2 == v2.cend();
+    return result;
 }
-
 int main(int argc, char const *argv[])
 {
-    std::vector<int> v1{2,1,1,2};
-    std::vector<int> v2{0,1,1,2,3,5,8};
-    std::cout << (isPrefix(v2,v1) ? "yes\n" : "no\n") << std::endl;
+    long j = fact(12);
+    std::cout << "12! = " << j << std::endl;
     return 0;
 }
