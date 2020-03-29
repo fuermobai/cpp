e1e167424a80f532c10146d19fc6fb0f732b8e21
fuermobai <fuermobai@gmail.com>
2020-03-29 17:38:47 +0800

    修改练习6.7

--- num1.cpp
+++ num1.cpp
@@ -12,7 +12,8 @@ int main(int argc, char const *argv[])
 {
     for (size_t i = 0; i != 10; ++i)
     {
-        std::cout << cycle() << std::endl;
+        std::cout << cycle() << " ";
     }
+    std::cout << std::endl;
     return 0;
 }
