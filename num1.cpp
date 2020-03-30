05d854c46f9bf62a52b6e22732878b3aeb29e405
fuermobai <fuermobai@gmail.com>
2020-03-30 22:52:59 +0800

    使用引用和指针，进行a、b值的互换。

--- num1.cpp
+++ num1.cpp
@@ -2,18 +2,28 @@
 #include <vector>
 #include <string>
 
-size_t cycle()
+void exchangevalue(int * val1,int * val2)
 {
-    static size_t num1 = 0;
-    return num1++;
+    int value;
+    value = *val1;
+    *val1 = *val2;
+    *val2 = value;
 }
 
+void exchangenothird(int &a,int &b)
+{
+    a = a + b;
+    b = a - b;
+    a = a - b;
+}
 int main(int argc, char const *argv[])
 {
-    for (size_t i = 0; i != 10; ++i)
-    {
-        std::cout << cycle() << " ";
-    }
-    std::cout << std::endl;
+    int input1 = 0,input2 = 0;
+    std::cout << "input two numbers:" << std::endl;
+    std::cin >> input1 >> input2 ;
+    std::cout << " input1 : " << input1 << " input2 : " << input2 << std::endl;
+    //exchangevalue(&input1,&input2);
+    exchangenothird(input1,input2);
+    std::cout << " new1 is " << input1 << " new2 is " << input2 << std::endl;
     return 0;
 }
