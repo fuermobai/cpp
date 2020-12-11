8bbc6e392afee4426d87f5b087a6f80e476f51f6
fuermobai <fuermobai@gmail.com>
2020-12-11 13:19:47 +0800

    update 随机数字注释

--- random_numbers_create.cpp
+++ random_numbers_create.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 int main()
 {
-    int start = clock();
+    int start = clock();//配合15行计算本程序运行时间
     srand((unsigned long)time(NULL));
     unsigned long i,j;
     for (size_t i = 0; i < 10000000; ++i)//生成一千万个无符号随机数
@@ -12,6 +12,6 @@ int main()
         std::cout<< /*"随机数: "<<*/  j << std::endl;
     }
 //此程序生成文件大小约104MB    
-    printf("%.5lf\n",double(clock()-start)/CLOCKS_PER_SEC);
+    printf("%.5lf\n",double(clock()-start)/CLOCKS_PER_SEC);//配合6行计算本程序运行时间
     return 0;
 }
