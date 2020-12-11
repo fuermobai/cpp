de98d245379ec96c12cc262b07eac90b0d5995a4
fuermobai <fuermobai@gmail.com>
2020-12-11 11:20:50 +0800

    gitignore,更改随机数字运行时间精度为小数点后5位

--- random_numbers_create.cpp
+++ random_numbers_create.cpp
@@ -1,7 +1,17 @@
+#include <iostream>
 #include <ctime>
+#include <cstdlib>
 int main()
 {
-
     int start = clock();
-    printf("%.3lf\n",double(clock()-start)/CLOCKS_PER_SEC);
+    srand((unsigned long)time(NULL));
+    long i,j;
+    for (size_t i = 0; i < 10000000; ++i)
+    {
+        j = rand();
+        std::cout<< /*"随机数: "<<*/  j << std::endl;
+    }
+    
+    printf("%.5lf\n",double(clock()-start)/CLOCKS_PER_SEC);
+    return 0;
 }
