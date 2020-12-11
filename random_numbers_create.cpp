e1c6172cba902ffefef8bd93fa71207fb2ce9e81
fuermobai <fuermobai@gmail.com>
2020-12-11 11:26:49 +0800

    update commits

--- random_numbers_create.cpp
+++ random_numbers_create.cpp
@@ -5,13 +5,13 @@ int main()
 {
     int start = clock();
     srand((unsigned long)time(NULL));
-    long i,j;
-    for (size_t i = 0; i < 10000000; ++i)
+    unsigned long i,j;
+    for (size_t i = 0; i < 10000000; ++i)//生成一千万个无符号随机数
     {
         j = rand();
         std::cout<< /*"随机数: "<<*/  j << std::endl;
     }
-    
+//此程序生成文件大小约104MB    
     printf("%.5lf\n",double(clock()-start)/CLOCKS_PER_SEC);
     return 0;
 }
