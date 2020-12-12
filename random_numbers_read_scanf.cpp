ba329c58b7955573a76cc96a2c4ca312d8e7d832
fuermobai <fuermobai@gmail.com>
2020-12-12 21:39:47 +0800

    scanf 读取1000万个随机数字,用时2.71646s

---
+++ random_numbers_read_scanf.cpp
@@ -0,0 +1,21 @@
+/*读取1000万个随机数程序*/
+
+#include <ctime>
+#include <iostream>
+
+const int MAXN = 10000000;
+int numbers[MAXN];
+
+void scanf_read(){
+    freopen("data.txt", "r", stdin);
+    for (size_t i = 0; i < MAXN; ++i)
+    {
+        std::cin >> numbers[i];
+    }
+}
+int main(int argc, const char** argv) {
+    int start = clock();
+    scanf_read();
+    printf("%.5lf\n",double(clock() - start)/CLOCKS_PER_SEC);
+    return 0;
+}
