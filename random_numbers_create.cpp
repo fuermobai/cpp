9fd114be3467f6895abe4652864b75cf957421b5
fuermobai <fuermobai@gmail.com>
2020-12-12 17:44:58 +0800

    修改为输出文件而不是使用>>运算符进行文件输出

--- random_numbers_create.cpp
+++ random_numbers_create.cpp
@@ -1,17 +1,21 @@
+#include <cstdio>
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-int main()
+using namespace std;
+int main(int argc, const char** argv) 
 {
-    int start = clock();//配合15行计算本程序运行时间
+    int start = clock();//配合19行计算本程序运行时间
     srand((int)time(NULL));
     int i,j;
+    freopen("newdata.txt","w",stdout);
     for (size_t i = 0; i < 10000000; ++i)//生成一千万个无符号随机数
     {
         j = rand();
-        std::cout<< /*"随机数: "<<*/  j << std::endl;
+        cout<< /*"随机数: "<<*/  j << endl;
     }
 //此程序生成文件大小约104MB    
-    printf("%.5lf\n",double(clock()-start)/CLOCKS_PER_SEC);//配合6行计算本程序运行时间
+    printf("%.5lf\n",double(clock()-start)/CLOCKS_PER_SEC);//配合8行计算本程序运行时间
+    fclose(stdout);
     return 0;
 }
