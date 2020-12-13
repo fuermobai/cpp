26efceb668391306f7d8e8b215a9cc551864ca17
fuermobai <fuermobai@gmail.com>
2020-12-13 13:57:28 +0800

    fread 用时0.26107s

--- random_numbers_read.cpp
+++ random_numbers_read.cpp
@@ -14,7 +14,7 @@ void scanf_read(){
     }
 }
 
-void cin_read(){
+void cin_read_nosync(){
     freopen("data.txt", "r", stdin);
     std::ios::sync_with_stdio(false);
     for (size_t i = 0; i < MAXN; ++i)
@@ -22,9 +22,39 @@ void cin_read(){
         std::cin >> numbers[i];
     }
 }
+
+const int MAXS = 60*1024*1024;
+char buf[MAXS];
+
+void analyse(char *buf,int len = MAXS){
+    int i;
+    numbers[i=0]=0;
+    for (char *p = buf; *p && p-buf < len; ++p)
+    {
+        if (*p == ' ')
+        {
+            numbers[++i]=0;
+        }
+        else
+        {
+            numbers[i]=numbers[i]*10 + *p - '0';
+        }
+    }
+}
+
+void fread_analysis(){
+    freopen("data.txt", "rb", stdin);
+    int len = fread(buf,1,MAXS,stdin);
+    buf[len] = '\0';
+    analyse(buf,len);
+}
+
+
+
 int main(int argc, const char** argv) {
     int start = clock();
-    cin_read();
+    //cin_read_nosync();
+    fread_analysis();
     printf("%.5lf\n",double(clock() - start)/CLOCKS_PER_SEC);
     return 0;
 }
